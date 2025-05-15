9878131d33ea9356d7d43404f0f35dbabd154c3e
atodd93 <atodd93@live.com>
2025-05-15 17:10:54 -0400

    Argument support added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, char_instances, kv_instances
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,7 +8,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     print('============ BOOKBOT ============')
     print(f'Analyzing book found at {filepath}...')
